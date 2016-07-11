f77ea2f2a08482096596ec278242dccb498ad78b
John Wästerlund <grunka@gmail.com>
2016-07-11 21:02:20 +0200

    Initial implementations, still want to split them up a bit better

---
+++ src/main/java/com/grunka/xorshift/XORShiftRandom.java
@@ -0,0 +1,152 @@
+package com.grunka.xorshift;
+
+import java.util.Random;
+
+public class XORShiftRandom {
+    private abstract static class XORShiftBase extends Random {
+        private long currentRandomBits = 0;
+        private int currentRemainingRandomBits = 0;
+
+        public XORShiftBase() {
+            super();
+        }
+
+        public XORShiftBase(long seed) {
+            super(seed);
+        }
+
+        @Override
+        public synchronized void setSeed(long seed) {
+            if (seed == 0) {
+                throw new IllegalArgumentException("Seed cannot be zero");
+            }
+            reset(seed);
+            currentRemainingRandomBits = 0;
+        }
+
+        @Override
+        protected synchronized int next(int bits) {
+            if (bits > 32) {
+                throw new IllegalArgumentException("Requested bits greater than 32");
+            }
+            int result = 0;
+            int bitsStillToTake = bits;
+            while (bitsStillToTake > 0) {
+                ensureBits();
+                int bitsToTake;
+                if (bitsStillToTake <= currentRemainingRandomBits) {
+                    bitsToTake = bitsStillToTake;
+                    bitsStillToTake = 0;
+                } else {
+                    bitsToTake = currentRemainingRandomBits;
+                    bitsStillToTake = bitsStillToTake - currentRemainingRandomBits;
+                }
+
+                int mask = ((1 << 31) >> (bitsToTake - 1)) >>> (32 - bitsToTake);
+                result = result | ((int) (currentRandomBits >>> (currentRemainingRandomBits - bitsToTake))) & mask;
+                result = result << bitsStillToTake;
+
+                currentRemainingRandomBits -= bitsToTake;
+            }
+            return result;
+        }
+
+        private void ensureBits() {
+            if (currentRemainingRandomBits == 0) {
+                currentRemainingRandomBits = 64;
+                currentRandomBits = next64Bits();
+            }
+        }
+
+        abstract void reset(long seed);
+        abstract long next64Bits();
+    }
+
+    private static class Star64 extends XORShiftBase {
+        private long x;
+
+        public Star64() {
+            super();
+        }
+
+        public Star64(long seed) {
+            super(seed);
+        }
+
+        @Override
+        public void reset(long seed) {
+            x = seed;
+        }
+
+        @Override
+        public long next64Bits() {
+            x ^= x >>> 12; // a
+            x ^= x << 25;  // b
+            x ^= x >>> 27; // c
+            return x * 2685821657736338717L;
+        }
+    }
+
+    private static class Plus128 extends XORShiftBase {
+        private long[] s;
+
+        public Plus128() {
+            super();
+        }
+
+        public Plus128(long seed) {
+            super(seed);
+        }
+
+        @Override
+        public void reset(long seed) {
+            s = expandSeed(seed, 2);
+        }
+
+        @Override
+        public long next64Bits() {
+            long x = s[0];
+            final long y = s[1];
+            s[0] = y;
+            x ^= x << 23; // a
+            s[1] = x ^ y ^ (x >>> 17) ^ (y >>> 26); // b, c
+            return s[1] + y;
+        }
+    }
+
+    private XORShiftRandom() {
+    }
+
+    public static Random create64Star() {
+        return new XORShiftRandom.Star64();
+    }
+
+    public static Random create64Star(long seed) {
+        return new XORShiftRandom.Star64(seed);
+    }
+
+    public static Random create128Plus() {
+        return new XORShiftRandom.Plus128();
+    }
+
+    public static Random create128Plus(long seed) {
+        return new XORShiftRandom.Plus128(seed);
+    }
+
+    private static long[] expandSeed(long seed, int length) {
+        if (seed == 0) {
+            throw new IllegalArgumentException("Seed not allowed to be zero");
+        }
+        if (length < 1) {
+            throw new IllegalArgumentException("Requested seed length too short");
+        }
+        long[] s = new long[length];
+        s[0] = seed;
+        final int w = Long.SIZE;
+        final long f = 6364136223846793005L;
+        for (int i = 1; i < s.length; i++) {
+            s[i] = f * (s[i - 1] ^ (s[i - 1] >>> (w - 2))) + i;
+        }
+        return s;
+    }
+}
