f9802cd980477772b14b9f79e13035b9c3b9bb31
John Wästerlund <grunka@gmail.com>
2016-07-11 21:02:47 +0200

    A small test that just dumps a file that can be used to test the output

---
+++ src/test/java/com/grunka/xorshift/XORShiftRandomTest.java
@@ -0,0 +1,28 @@
+package com.grunka.xorshift;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Random;
+
+@Ignore
+public class XORShiftRandomTest {
+
+    @Test
+    public void name() throws Exception {
+        Random random = XORShiftRandom.create128Plus();
+        Path path = Paths.get("random.out");
+        Files.deleteIfExists(path);
+        Files.createFile(path);
+        System.out.println("path.toRealPath() = " + path.toRealPath());
+        byte[] fourK = new byte[4 * 1024];
+        for (int i = 0; i < 1024 * 1024; i++) {
+            random.nextBytes(fourK);
+            Files.write(path, fourK, StandardOpenOption.APPEND);
+        }
+    }
+}
